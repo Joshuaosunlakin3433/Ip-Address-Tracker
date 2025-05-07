1b7c26899c04a07534e4d2ed6dda7c4129a1a9e5
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-05-07 17:39:26 +0100

    updated IP Address Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { IoIosArrowForward } from "react-icons/io";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { SimpleMap } from "./components/SimpleMap";
 
 type ipAddressData = {
   ip: string;
-  city: string;
   country: string;
-  timezone?: number;
-  location: string;
+  location: {
+    country: string;
+    region: string;
+    timezone: string;
+    lat: number;
+    lng: number;
+  };
   org: string;
   isp: string;
 };
@@ -22,35 +27,36 @@ const App = () => {
     setIpAddress(event.target.value);
   };
 
-  useEffect(() => {
-    const fetchingIpAddress = async () => {
-      try {
-        const res = await fetch(
-          `https://geo.ipify.org/api/v2/country?apiKey=at_QUJo41E37FFQCkmrvBSZOWw3WzKxA&ipAddress=${ipAddress}`
-        );
-        const data = await res.json();
-        console.log(data);
-        setIpAddressData(data);
-      } catch (error) {
-        console.error("Error fetching IP address:", error);
-      }
-    };
-    fetchingIpAddress();
-  }, [ipAddress]);
+  const fetchingIpAddress = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_QUJo41E37FFQCkmrvBSZOWw3WzKxA&ipAddress=${ipAddress}`
+      );
+      const data = await res.json();
+      console.log("data", data);
+      setIpAddressData(data);
+    } catch (error) {
+      console.error("Error fetching IP address:", error);
+    }
+  };
 
   return (
-    <div className="relative">
+    <main className="relative">
       <img
         src="./src/assets/images/pattern-bg-desktop.png"
         alt=""
-        className="w-full h-60"
+        className="w-full h-60 object-cover"
       />
       <div>
         <div className="absolute top-0 mt-10 mx-auto w-full flex flex-col justify-center items-center">
           <h1 className="text-white text-lg text center mb-5">
             IP Address Tracker
           </h1>
-          <form className="flex flex-start items-center">
+          <form
+            onSubmit={fetchingIpAddress}
+            className="flex flex-start items-center"
+          >
             <input
               onChange={handleInputChange}
               type="text"
@@ -73,7 +79,13 @@ const App = () => {
               <p className="text-xs uppercase text-gray-500 font-medium">
                 Location
               </p>
-              <h2>{ipAddressData?.location.region}, {ipAddressData?.location.country}</h2>
+              <h2>
+                {ipAddressData
+                  ? ipAddressData.location.region +
+                    ", " +
+                    ipAddressData?.location.country
+                  : "Location"}
+              </h2>
               <hr className="border border-gray-500/20 " />
             </div>
             <div className="w-70 md:max-w-50">
@@ -85,15 +97,23 @@ const App = () => {
             </div>
             <div className="w-70 md:max-w-50">
               <p className="text-xs uppercase text-gray-500 font-medium">ISP</p>
-              <h2>GilRevv Ltd.</h2>      
+              <h2>GilRevv Ltd.</h2>
               {/* {ipAddressData?.isp || "GilRevv"} */}
               <hr className="border border-gray-500/20" />
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {ipAddressData && (
+        <SimpleMap
+          latitude={ipAddressData.location.lat}
+          longitude={ipAddressData.location.lng}
+          zoom={13}
+        />
+      )}
+    </main>
   );
 };
- 
+
 export default App;
---
+++ src/components/SimpleMap.tsx
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Marker, Popup } from "react-leaflet";
+
+type Props = {
+  longitude: number;
+  latitude: number;
+  zoom: number;
+};
+
+export const SimpleMap = ({ latitude, longitude }: Props) => {
+  const mapRef = useRef(null);
+
+  return (
+    // Make sure you set the height and width of the map container otherwise the map won't show
+    <MapContainer
+      center={[latitude, longitude]}
+      zoom={13}
+      scrollWheelZoom={false}
+      ref={mapRef}
+      style={{ height: "100vh", width: "100vw", position: "absolute" }}
+      className="-z-[10000]"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            Latitude: {latitude}, Longitude: {longitude}
+          </Popup>
+        </Marker>
+      }
+    </MapContainer>
+  );
+};
